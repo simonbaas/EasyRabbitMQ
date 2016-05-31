f233bf5db5793c9e651ab66c9a54348c979d3b69
simonbaas <simon.baas@gmail.com>
2016-05-31 09:17:20 +0200

    Added Register method to enable registration of message handlers in BuiltInHandlerActivator

--- src/EasyRabbitMQ/Infrastructure/BuiltInHandlerActivator.cs
+++ src/EasyRabbitMQ/Infrastructure/BuiltInHandlerActivator.cs
@@ -3,15 +3,24 @@ using System.Collections.Concurrent;
 
 namespace EasyRabbitMQ.Infrastructure
 {
-    internal class BuiltInHandlerActivator : IHandlerActivator
+    public class BuiltInHandlerActivator : IHandlerActivator
     {
-        private readonly ConcurrentDictionary<Type, dynamic> _types = new ConcurrentDictionary<Type, dynamic>(); 
+        private readonly ConcurrentDictionary<Type, dynamic> _types = new ConcurrentDictionary<Type, dynamic>();
+
+        public void Register<TMessage, THandler>(Func<THandler> func) where THandler : IHandleMessages<TMessage>
+        {
+            _types.AddOrUpdate(typeof (THandler), type => func(), (type, o) => func());
+        }
 
         public THandler Get<TMessage, THandler>(ITransactionContext transactionContext) where THandler : IHandleMessages<TMessage>
         {
             if (transactionContext == null) throw new ArgumentNullException(nameof(transactionContext));
 
-            var handler = _types.GetOrAdd(typeof (THandler), _ => Activator.CreateInstance<THandler>());
+            dynamic handler;
+            if (!_types.TryGetValue(typeof (THandler), out handler))
+            {
+                throw new InvalidOperationException($"No handler registered for type '{typeof(THandler)}'. Use Register method to register handlers.");
+            }
 
             transactionContext.OnDisposed(() =>
             {
