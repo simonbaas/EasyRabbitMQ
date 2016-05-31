1ffbf65ab59e278dee71602c5b0cf1ad579076e1
simonbaas <simon.baas@gmail.com>
2016-05-31 08:24:31 +0200

    Added logging implementation for Log4Net

---
+++ src/EasyRabbitMQ.Log4Net/Log4NetConfigurerExtension.cs
@@ -0,0 +1,14 @@
+﻿using EasyRabbitMQ.Configuration;
+
+namespace EasyRabbitMQ.Log4Net
+{
+    public static class Log4NetConfigurerExtension
+    {
+        public static EasyRabbitMQConfigurer UseLog4Net(this EasyRabbitMQConfigurer configurer)
+        {
+            configurer.Use(new Log4NetLoggerFactory());
+
+            return configurer;
+        }
+    }
+}
---
+++ src/EasyRabbitMQ.Log4Net/Log4NetLoggerFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EasyRabbitMQ.Logging;
+
+namespace EasyRabbitMQ.Log4Net
+{
+    public class Log4NetLoggerFactory : AbstractLoggerFactory
+    {
+        public override ILogger GetLogger(Type type)
+        {
+            return new Log4NetLogger(type);
+        }
+    }
+}
